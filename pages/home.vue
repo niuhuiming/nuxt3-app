05b9e3d277293f8cde3f25ff04fc478607ecc22b
nhm <3059350051@qq.com>
2023-03-28 11:09:43 +0800

    feat 测试发送请求

---
+++ pages/home.vue
@@ -0,0 +1,10 @@
+<script setup>
+const { data } = await useAsyncData('ssr', () => $fetch('http://43.140.248.221:3000/dj/program?rid=336355127'))
+const { programs } = toRaw(data.value)
+console.log(programs)
+</script>
+
+<template>
+  Page visits:
+  <div v-for="(item, index) in programs" :key="index">{{ item.name }}</div>
+</template>
